1a7e8b65d9e0aa01690cb5261b5205937b9b265b
William Gillespie <wgilles1@emich.edu>
2017-02-14 17:25:45 -0500

    Made a correct update inventory function.

--- hw06PHP_Arrays/inventory_funcs.php
+++ hw06PHP_Arrays/inventory_funcs.php
@@ -28,4 +28,30 @@ function init_item_array(){
   );
   return $array;
 }
- ?>
+
+function init_orders(){
+    $arr = array();
+    $arr[] = array(
+      "name" => "bat",
+      "quantity" => 4
+    );
+    $arr[] = array(
+      "name" => "player",
+      "quantity" => -1
+    );
+    return $arr;
+}
+
+function update_inventory_for_order($inventory, $order){
+  for($i = 0; $i < count($order);$i++){
+    $order_name = $order[$i]['name'];
+    $order_quantity = $order[$i]['quantity'];
+    for($j = 0;$j < count($inventory);$j++){
+      if($inventory[$j]['name'] == $order_name){
+        $inventory[$j]['in_stock'] += $order_quantity;
+      }
+    }
+  }
+  return $inventory;
+}
+?>
