d94e9bd7b027e75e694be7ff3c0ac33041b27147
William Gillespie <wgilles1@emich.edu>
2017-02-14 14:15:37 -0500

    echos back the item and the value of all of its stock.

---
+++ hw06PHP_Arrays/part2.php
@@ -0,0 +1,20 @@
+<?php
+include('init_array_item.php');
+
+function value_for_stock($inventory_item) {
+  return array($inventory_item['name'], $inventory_item['in_stock'] * $inventory_item['per_item_cost']);
+}
+
+$item_array = init_item_array();
+
+$item_stock_values = array_map("value_for_stock", $item_array);
+
+$arr_hashmap = array();
+foreach($item_stock_values as $name_value){
+  $arr_hashmap[$name_value[0]] = $name_value[1];
+}
+
+echo json_encode($arr_hashmap);
+
+
+ ?>
