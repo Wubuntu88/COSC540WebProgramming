c72dbae3e15b3813883b6740b7ac2d4dca1cf515
William Gillespie <wgilles1@emich.edu>
2017-03-02 16:11:21 -0500

    Susan's course archive page with the header and footer included from other files.

---
+++ hw07_php_include/courseArchive.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!-- saved from url=(0050)http://emunix.emich.edu/~haynes/courseArchive.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>haynes' Course Archive</title>
+  <!--<base href="http://emunix.emich.edu/~haynes/public_html">--><base href=".">
+
+  <link rel="stylesheet" href="./courseArchive_files/EMUstyle2015.css" type="text/css">
+  <style type="text/css">
+   th, td { padding-left: 5px;
+   			padding-right: 5px;
+   			padding-top: 2px;
+   			padding-bottom: 2px; }
+  </style>
+
+</head>
+
+<body>
+
+  <?php include("header.html"); ?>
+
+<br>
+
+<h3 align="center"> smh: Course Archive Site </h3>
+
+<h5 align="center">
+							Use at your own risk, old course sites are not maintained </h5>
+
+<br> <br>
+<hr width="50%">
+<br> <br>
+
+<table border="1">
+<tbody><tr> <td>COSC 311</td>     <td> <a href="http://emunix.emich.edu/~haynes/311/">Algorithms &amp; Data Structures</a></td>  </tr>
+<tr> <td>COSC 457/557</td>     <td> <a href="http://emunix.emich.edu/~haynes/457/">Computer Game Programming</a></td>  </tr>
+<tr> <td>COSC 681</td>     <td> <a href="http://emunix.emich.edu/~haynes/681/fa16">Simulation</a></td>  </tr>
+<tr> <td>COSC 231</td>     <td> <a href="http://emunix.emich.edu/~haynes/231/">Internet Based Programming</a></td>  </tr>
+<tr> <td>COSC 341/342</td> <td> <a href="http://emunix.emich.edu/~haynes/341/">Programming Languages </a></td>    </tr>
+<tr> <td>COSC 612</td>  <td><a href="http://emunix.emich.edu/~haynes/612/"> Parallel Algorithms</a></td> </tr>
+<tr> <td>COSC 120</td>  <td><a href="http://emunix.emich.edu/~haynes/120/"> Computational Principles for Mathematics and Sciences</a></td>       </tr>
+<tr> <td>COSC 671</td>  <td><a href="http://emunix.emich.edu/~haynes/671/">Advanced Database Topics</a></td></tr>
+<tr> <td>COSC 422</td>  <td><a href="http://emunix.emich.edu/~haynes/422/"> Introduction to Microprocessors</a> </td> </tr>
+<tr> <td>COSC 221</td>  <td><a href="http://emunix.emich.edu/~haynes/221/">Computer Organization I</a></td></tr>
+<tr> <td>COSC 511</td>		<td><a href="http://emunix.emich.edu/~haynes/511/">Analysis of Algorithms</a></td></tr>
+<tr> <td>COSC 625</td>		<td><a href="http://emunix.emich.edu/~haynes/625/">Real Time Processing</a></td></tr>
+<tr> <td> COSC 445 </td>	<td><a href="http://emunix.emich.edu/~haynes/445/">Compiler Construction</a></td></tr>
+
+<tr> <td>COSC 612</td> 		<td> <a href="http://emunix.emich.edu/~haynes/612/">Parallel Algorithms</a></td></tr>
+
+<tr> <td>COSC 671 </td> 	<td><a href="http://emunix.emich.edu/~haynes/671/"> Advanced Topics in DBMS </a> </td>	</tr>
+
+<tr><td>COSC 321</td>		<td><a href="http://emunix.emich.edu/~haynes/321/"> Computer Organization 2 </a></td>	 </tr>
+<tr><td>COSC 374 (379)</td>	<td><a href="http://emunix.emich.edu/~haynes/374/"> Applied Cryptography </a></td></tr>
+
+<tr> <td>COSC 211 </td> 	<td><a href="http://emunix.emich.edu/~haynes/211/"> Computer Science 2 (Java) </a> </td>	</tr>
+<tr> <td>COSC 471 </td> 	<td><a href="http://emunix.emich.edu/~haynes/471/"> Database Principles </a> </td>	</tr>
+
+<tr> <td>COSC 625 </td>		<td><a href="http://emunix.emich.edu/~haynes/625/"> Real Time Processing </a> </td>	</tr>
+
+<tr><td>COSC 246 </td>		<td><a href="http://emunix.emich.edu/~haynes/246/"> Programming in C++</a></td>		</tr>
+
+
+
+</tbody></table>
+
+
+<br> <br>
+<hr>
+<br> <br>
+
+<?php include("footer.html") ?>
+
+
+
+
+</body></html>
