204adfb5d2fa21e2abc2d7b590a3e7d7e0d89166
William Gillespie <wgilles1@emich.edu>
2017-03-18 23:16:50 -0400

    Finished all of the insert values into tables statements.

--- hw09MidtermProject/create_tables.sql
+++ hw09MidtermProject/create_tables.sql
@@ -49,7 +49,7 @@ CREATE TABLE PrivateOwner(
     ownerNo varchar(8) NOT NULL,
     fName varchar(20) NOT NULL,
     lName varchar(20) NOT NULL,
-    address varchar(20),
+    address varchar(30),
     telNo varchar(20),
     eMail varchar(30),
     password varchar(20) NOT NULL,
@@ -91,26 +91,10 @@ CREATE TABLE Viewing(
 INSERT INTO Branch
 (branchNo, street, city, postcode)
 VALUES 
-('b005', '22 Deer Rd', 'London', 'SW14EH');
-
-INSERT INTO Branch
-(branchNo, street, city, postcode)
-VALUES 
-('b007', '16 Argyll St', 'Aberdeen', 'AB23SU');
-
-INSERT INTO Branch
-(branchNo, street, city, postcode)
-VALUES 
-('b003', '163 Main St', 'Glasgow', 'G119QX');
-
-INSERT INTO Branch
-(branchNo, street, city, postcode)
-VALUES 
-('b004', '32 Manse Rd', 'Bristol', 'BS991NZ');
-
-INSERT INTO Branch
-(branchNo, street, city, postcode)
-VALUES 
+('b005', '22 Deer Rd', 'London', 'SW14EH'),
+('b007', '16 Argyll St', 'Aberdeen', 'AB23SU'),
+('b003', '163 Main St', 'Glasgow', 'G119QX'),
+('b004', '32 Manse Rd', 'Bristol', 'BS991NZ'),
 ('b002', '64 Clover Dr', 'London', 'NW106EU');
 
 /* INSERT INTO Branch */
@@ -118,31 +102,11 @@ VALUES
 INSERT INTO Staff
 (staffNo, fName, lName, position, sex, DOB, salary, branchNo)
 VALUES 
-('SL21', 'John', 'White', 'Manager', 'M', '1945-10-1', 30000, 'b005');
-
-INSERT INTO Staff
-(staffNo, fName, lName, position, sex, DOB, salary, branchNo)
-VALUES 
-('SG37', 'Ann', 'Beech', 'Assistant', 'F', '1960-11-10', 12000, 'b003');
-
-INSERT INTO Staff
-(staffNo, fName, lName, position, sex, DOB, salary, branchNo)
-VALUES 
-('SG14', 'David', 'Ford', 'Supervisor', 'M', '1958-03-24', 18000, 'b003');
-
-INSERT INTO Staff
-(staffNo, fName, lName, position, sex, DOB, salary, branchNo)
-VALUES 
-('SA9', 'Mary', 'Howe', 'Assistant', 'F', '1970-01-19', 9000, 'b007');
-
-INSERT INTO Staff
-(staffNo, fName, lName, position, sex, DOB, salary, branchNo)
-VALUES 
-('SG5', 'Susan', 'Brand', 'Manager', 'F', '1940-06-03', 24000, 'b003');
-
-INSERT INTO Staff
-(staffNo, fName, lName, position, sex, DOB, salary, branchNo)
-VALUES 
+('SL21', 'John', 'White', 'Manager', 'M', '1945-10-1', 30000, 'b005'),
+('SG37', 'Ann', 'Beech', 'Assistant', 'F', '1960-11-10', 12000, 'b003'),
+('SG14', 'David', 'Ford', 'Supervisor', 'M', '1958-03-24', 18000, 'b003'),
+('SA9', 'Mary', 'Howe', 'Assistant', 'F', '1970-01-19', 9000, 'b007'),
+('SG5', 'Susan', 'Brand', 'Manager', 'F', '1940-06-03', 24000, 'b003'),
 ('SL41', 'Julie', 'Lee', 'Assistant', 'F', '1965-06-3', 9000, 'b005');
 
 /* INSERT INTO Client */
@@ -151,30 +115,39 @@ VALUES
 INSERT INTO Client
 (ClientNo, fName, lName, telNo, prefType, maxRent, eMail)
 VALUES 
-('CR76', 'John', 'Kay', '0207-774-5632', 'Flat', 425, 'john.kay@gmail.com');
-
-INSERT INTO Client
-(ClientNo, fName, lName, telNo, prefType, maxRent, eMail)
-VALUES 
-('CR56', 'Aline', 'Stewart', '0141-848-1825', 'Flat', 350, 'astewart@hotmail.com');
-
-INSERT INTO Client
-(ClientNo, fName, lName, telNo, prefType, maxRent, eMail)
-VALUES 
-('CR74', 'Mike', 'Ritchie', '01475-392178', 'House', 750, 'mritchie01@yahoo.co.uk');
-
-INSERT INTO Client
-(ClientNo, fName, lName, telNo, prefType, maxRent, eMail)
-VALUES 
+('CR76', 'John', 'Kay', '0207-774-5632', 'Flat', 425, 'john.kay@gmail.com'),
+('CR56', 'Aline', 'Stewart', '0141-848-1825', 'Flat', 350, 'astewart@hotmail.com'),
+('CR74', 'Mike', 'Ritchie', '01475-392178', 'House', 750, 'mritchie01@yahoo.co.uk'),
 ('CR62', 'Mary', 'Tregear', '01224-196720', 'Flat', 600, 'maryt@hotmail.co.uk');
 
+/* INSERT INTO PrivateOwner */
+INSERT INTO PrivateOwner
+(OwnerNo, fName, lName, address, telNo,  eMail, password)
+VALUES 
+('CO46', 'Joe', 'Keogh', '2 Fergus Dr, Aberdeen AB27XW', '01224-861212', 'jkeogh@lhh.com', '12345'),
+('CO87', 'Carol', 'Farrel', '6 Archray St, Glasgow G329DX', '0141-357-7419', 'cfarrel@gmail.com', '12345'),
+('CO40', 'Tina', 'Murphy', '36 Well St, Glasgow G42', '0141-943-1728', 'tinam@hotmail.com', '12345'),
+('CO93', 'Tony', 'Shaw', '12 Park Pl, Glasgow G40QR', '0141-225-7025', 'tony.shaw@ark.com', '12345');
 
+/* INSERT INTO PropertyForRent */
+INSERT INTO PropertyForRent
+(propertyNo, street, city, postcode, type, rooms, rent, ownerNo, staffNo, branchNo)
+VALUES 
+('PA14', '16 Holhead', 'Aberdeen', 'AB755U', 'House', 6, 650, 'CO46', 'SA9', 'b007'),
+('PL94', '6 Argyll St', 'London', 'NW2', 'Flat', 4, 400, 'CO87', 'SL41', 'b005'),
+('PG4', '6 Lawrence St', 'Glasgow', 'G119QX', 'Flat', 3, 350, 'CO40', 'SG5', 'b003'),
+('PG36', '2 Manor Rd', 'Glasgow', 'G324QX', 'Flat', 3, 375, 'CO93', 'SG37', 'b003'),
+('PG21', '18 Dale Rd', 'Glasgow', 'G12', 'House', 5, 600, 'CO87', 'SG37', 'b003'),
+('PG16', '5 Novar Dr', 'Glasgow', 'G129AX', 'Flat', 4, 450, 'CO93', 'SG14', 'b003');
 
-
-
-
-
-
+INSERT INTO Viewing
+(clientNo, propertyNo, viewDate, comments)
+VALUES
+('CR56', 'PA14', '2013-05-13', 'too small'),
+('CR76', 'PG4', '2013-04-13', 'too remote'),
+('CR56', 'PG4', '2013-04-13', 'bad views'),
+('CR62', 'PA14', '2013-05-13', 'no dining room'),
+('CR56', 'PG36', '2013-04-13', 'small interior');
 
 
 
